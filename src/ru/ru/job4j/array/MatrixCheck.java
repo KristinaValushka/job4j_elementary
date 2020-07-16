7d262e63820ee1795547a614f0926c1192658e07
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-15 22:45:46 -0700

    6.7.4. Выигрышные комбинации в сокобан [#309807]

--- src/ru/ru/job4j/array/MatrixCheck.java
+++ src/ru/ru/job4j/array/MatrixCheck.java
@@ -35,9 +35,11 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-                        if (board[i][i] == 'X') {
-                result = true;
-                break;
+            if (board[i][i] == 'X') {
+                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+                    result = true;
+                    break;
+                }
             }
         }
         return result;
