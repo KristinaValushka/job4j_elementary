858b12598dd94c4ed7ece9605991d9ef71d9e000
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-15 22:25:15 -0700

    6.8. Дефрагментация массива [#309827]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -1,25 +1,34 @@
 package ru.job4j.array;
 
+
 import static com.sun.tools.javac.jvm.ByteCodes.swap;
 
+
 public class Defragment {
-    public static String[] compress(String[] array) {
+         public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int nullIndex = index; /* указатель на null ячейку. */
-               /* переместить первую не null ячейку. Нужен цикл. */
-                if(array[index] != 0) {
-                    int notNullIndex = index;
-                    swap(array, nullIndex, notNullIndex);
+                int nullIndex = index;
+
+                for (int i = nullIndex + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        int notNullIndex = i;
+                        swap(array, nullIndex, notNullIndex);
+                        break;
+                    }
                 }
-
             }
-
             System.out.print(array[index] + " ");
         }
         return array;
     }
 
+    private static void swap(String[] array, int nullIndex, int notNullIndex) {
+        String save = array[nullIndex];
+        array[nullIndex] = array[notNullIndex];
+        array[notNullIndex] = save;
+
+    }
 
     public static void main(String[] args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
