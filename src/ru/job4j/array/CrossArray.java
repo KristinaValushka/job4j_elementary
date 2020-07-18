b721e567293ba507c295cbf3b8dd28b1b196be30
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-18 10:36:39 -0700

    2. Одинаковые элементы для массивов

---
+++ src/ru/job4j/array/CrossArray.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class CrossArray {
+
+        public static void printCrossEl(int[] left, int[] right) {
+            for (int row = 0; row < left.length; row++) {
+                for (int cell = 0; cell < right.length; cell++) {
+
+                    if (left[row] == right[cell]) {
+                        System.out.println(left[row]);
+                    }
+                }
+            }
+
+        }
+
+    public static void main(String[] args) {
+        CrossArray.printCrossEl(
+                new int[] {1, 3},
+                new int[] {2, 1}
+        );
+    }
+}
