dbf8eedb893a78fdb1e408a91564d3b56c1f63b0
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-20 18:58:45 -0700

    1. Чтение кода и базовые ошибки [#309979]

--- src/ru/job4j/array/CrossArray.java
+++ src/ru/job4j/array/CrossArray.java
@@ -1,11 +1,9 @@
 package ru.job4j.array;
 
 public class CrossArray {
-
         public static void printCrossEl(int[] left, int[] right) {
             for (int row = 0; row < left.length; row++) {
                 for (int cell = 0; cell < right.length; cell++) {
-
                     if (left[row] == right[cell]) {
                         System.out.println(left[row]);
                     }
@@ -13,7 +11,6 @@ public class CrossArray {
             }
 
         }
-
     public static void main(String[] args) {
         CrossArray.printCrossEl(
                 new int[] {1, 3},
