9c57ab8c81e10387f6abd283efe6fec58145c7d2
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-22 20:40:19 -0700

    1.7. Состояние объекта [#309973]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int rest) {
+        this.load = rest;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(85);
+        Battery restBattery = new Battery(5);
+        battery.exchange(restBattery);
+        System.out.println(battery.load);
+
+    }
+}
