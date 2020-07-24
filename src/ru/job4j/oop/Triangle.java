1a45ab25267925e1288f01b7a5f339438903cab0
KristinaValushka <krystsinavalushka@yahoo.com>
2020-07-24 13:15:32 -0700

    2.2. Рефакторинг - Площадь треугольника. [#309964]

---
+++ src/ru/job4j/oop/Triangle.java
@@ -0,0 +1,49 @@
+package ru.job4j.oop;
+
+import java.awt.*;
+
+import static java.lang.StrictMath.sqrt;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+       return a + b + c;
+    }
+
+
+    public boolean exist(double ab, double ac, double bc) {
+        return true;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+            /* написать формулу для расчета площади треугольника. */
+
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+
+    }
+}
