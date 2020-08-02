134f6900da4c9710084a1fb63bcdfade5c3edf09
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-01 18:45:22 -0700

    4. Переопределение [#309991]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -5,7 +5,7 @@ import ru.job4j.inheritance.ru.job4j.inheritance.TextReport;
       public class JSONReport extends TextReport {
             @Override
             public String generate(String name, String body) {
-                return "{ " + System.lineSeparator() + " " + name + "  " + ":" + " " + "\"name\"" + "," + System.lineSeparator() +
-                        " " + body + " " + ":" + "  "  + "\"body\"" + System.lineSeparator() + "}";
+                return "{ " + System.lineSeparator() + "  " + name + " " + ":" + " " + "\"name\"" + "," + System.lineSeparator() +
+                        "  " + body + " " + ":" + " "  + "\"body\"" + System.lineSeparator() + "}";
             }
         }
