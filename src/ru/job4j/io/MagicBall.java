b0b04c8cf4fb4b5222c1a1a2d56f9c2ea92eea03
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-08 09:21:40 -0700

    1. Чтение из консоли. Класс Scanner. [#309934]

--- src/ru/job4j/io/MagicBall.java
+++ src/ru/job4j/io/MagicBall.java
@@ -5,20 +5,17 @@ import java.util.Scanner;
 
 public class MagicBall {
 
-    public class RandomNumber{
-        int answer = new Random().nextInt(3);
-   if (answer == 0) {
-        System.out.println("Да");
-    } else if (answer == 1) {
-        System.out.println("Нет");
-    } else {
-        System.out.println("Может быть");
+          public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            String answerUser = input.nextLine();
+            int answer = new Random().nextInt(3);
+            if (answer == 0) {
+                System.out.println("Да");
+            } else if (answer == 1) {
+                System.out.println("Нет");
+            } else {
+                System.out.println("Может быть");
+            }
+        }
     }
-}
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("У тебя есть любимая игрушка?");
-        String answerUser = input.nextLine();
 
-    }
-}
--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner firstPlayer = new Scanner(System.in);
-        Scanner secondPlayer = new Scanner(System.in);
         int matches = 11;
+        int numberOfOnePlayer = Integer.valueOf(firstPlayer.nextLine());
+        
+        while (numberOfOnePlayer < 1 && numberOfOnePlayer > 3) {
+             numberOfOnePlayer = Integer.valueOf(firstPlayer.nextLine());
 
-        while (matches >= 0) {
-            System.out.println("Ходит первй игрок");
-            int numberOfOnePlayer = Integer.valueOf(firstPlayer.nextLine());
             if (numberOfOnePlayer == 1) {
                 matches = matches - numberOfOnePlayer;
                 System.out.println("Осталось " + matches + " спичек");
@@ -22,20 +22,7 @@ public class Matches {
                 matches = matches - numberOfOnePlayer;
                 System.out.println("Осталось " + matches + " спичек");
             }
-
-           System.out.println("Ходит второй игрок");
-            int numberOfTwoPlayer = Integer.valueOf(secondPlayer.nextLine());
-            if (numberOfOnePlayer == 1) {
-                matches = matches - numberOfTwoPlayer;
-                System.out.println("Осталось " + matches + " спичек");
-            } else if (numberOfOnePlayer == 2) {
-                matches = matches - numberOfTwoPlayer;
-                System.out.println("Осталось " + matches + " спичек");
-            } else {
-                matches = matches - numberOfTwoPlayer;
-                System.out.println("Осталось " + matches + " спичек");
-            }
+        }
 
         }
     }
-}
