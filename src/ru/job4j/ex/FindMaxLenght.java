83807e745939850765661a763235b27c99656bf4
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-15 18:06:57 -0700

    0.1. java.lang.NullPointerException [#309952]

--- src/ru/job4j/ex/FindMaxLenght.java
+++ src/ru/job4j/ex/FindMaxLenght.java
@@ -7,10 +7,11 @@ public class FindMaxLenght {
         for (int index = 0; index < shops.length; index++) {
             String el = shops[index];
             if (shops[index] != null) {
-                max = el.length();
+                if  (el.length() > max) {
+                    max = el.length();
+                }
             }
         }
         System.out.println("Max length : " + max);
     }
-
 }
