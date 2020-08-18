2fd037600ca16c09a52b0b5592f0854e72c712db
KristinaValushka <krystsinavalushka@yahoo.com>
2020-08-17 21:09:08 -0700

    0.5. Иерархия исключений и множественный catch. [#309956]

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -2,15 +2,23 @@ package ru.job4j.ex;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User userName = null;
         for (int i = 0; i < users.length; i++) {
-            if (users[i].equals("Kristina Valushka") {
-                return login;
+            if (users[i].equals(login)) {
+                userName = users[i];
+                System.out.println("Found user " + userName);
+            } else {
+                throw new UserNotFoundException();
             }
-        }
-        return null;
+        } return userName;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
+        user.isValid();
+        if (false) {
+            throw new UserInvalidException();
+        }
+
         return false;
     }
 
